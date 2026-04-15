186d85b27a76c0165dccbfc5941599bf7dcb180d
Datamik <radioarmy@outlook.com>
2026-04-16 00:48:31 +0400

    Add GSC verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css'
 import { JetBrains_Mono } from 'next/font/google'
 
+<head>
+  <meta name="google-site-verification" content="google-site-verification=DOTYPEMhnn9mXdgsg6QakJEmwlVB2dDuCts8Gly-vUc" />
+</head>
+
 const jetbrains = JetBrains_Mono({
   subsets: ['latin'],
   variable: '--font-jetbrains',
